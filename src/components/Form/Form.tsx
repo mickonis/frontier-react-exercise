9227983e8cb3c9f724ff1c2c667fb4eed36ddfa1
mickonis <mickonis.l@gmail.com>
2021-11-17 20:13:20 +0200

    added step counter

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,4 +1,5 @@
 import Input from 'components/Input/Input';
+import { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import './Form.scss';
 
@@ -8,6 +9,7 @@ interface FormProps {
 
 const Form = ({ job }: FormProps) => {
   const { sections } = job;
+  const [step, setStep] = useState(0);
   console.log(sections);
 
   const {
@@ -23,20 +25,30 @@ const Form = ({ job }: FormProps) => {
 
   console.log('form:', watch());
 
+  const activeSection = sections[step];
+
   return (
     <div className="form">
       <div className="form__body">
+        {step}
         <form action="">
-          {sections.map(section => (
-            <>
-              <div className="form-section">{section.title}</div>
-              {section.content.map(content => (
-                <div className="form-section__content">
-                  <Input content={content} control={control} />
-                </div>
-              ))}
-            </>
-          ))}
+          <>
+            <div className="form-section">{activeSection.title}</div>
+            {activeSection.content.map(content => (
+              <div className="form-section__content">
+                <Input content={content} control={control} />
+              </div>
+            ))}
+          </>
+
+          <button
+            onClick={e => {
+              e.preventDefault();
+              setStep(step + 1);
+            }}
+          >
+            Next
+          </button>
         </form>
       </div>
     </div>
