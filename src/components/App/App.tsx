ee4dc820b464759055d83d4fc181f791f387f4e5
mickonis <mickonis.l@gmail.com>
2021-11-18 15:59:50 +0200

    setting app theme

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -11,9 +11,22 @@ function App() {
   useEffect(() => {
     setFormInstructions(formInstructionsJson as Frontier.Job);
     setTotalSteps(formInstructionsJson.sections.length);
+    setAppTheme(formInstructionsJson.theme);
   }, []);
 
   return <div className="app">{formInstructions && <PhoneMockup />}</div>;
 }
+
+const setAppTheme = (theme: Frontier.Theme) => {
+  const documentStyle = document.documentElement.style;
+  const { primary_color, secondary_color, background_color, text_color } =
+    theme;
+  primary_color && documentStyle.setProperty('--primary-color', primary_color);
+  secondary_color &&
+    documentStyle.setProperty('--secondary-color', secondary_color);
+  background_color &&
+    documentStyle.setProperty('--background-color', background_color);
+  text_color && documentStyle.setProperty('--text-color', text_color);
+};
 
 export default App;
