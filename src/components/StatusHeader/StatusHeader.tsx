1bd246d15854cb89f336cf940abfe6497064959e
mickonis <mickonis.l@gmail.com>
2021-11-18 11:02:54 +0200

    added dynamic functionality

--- src/components/StatusHeader/StatusHeader.tsx
+++ src/components/StatusHeader/StatusHeader.tsx
@@ -1,13 +1,19 @@
+import { FormContext } from 'context/FormState';
+import { useContext } from 'react';
 import './StatusHeader.scss';
 
 const StatusHeader = () => {
+  const { currentStep, totalSteps } = useContext(FormContext);
+  const currentStepPresentation = currentStep + 1;
   return (
     <div className="status-header">
-      <div className="status-header__step-counter">Step 1 of 3</div>
+      <div className="status-header__step-counter">
+        Step {currentStepPresentation} of {totalSteps}
+      </div>
       <div className="status-header__progress-bar">
         <div
           className="status-header__progress-filler"
-          style={{ width: '33%' }}
+          style={{ width: `${(currentStepPresentation / totalSteps) * 100}%` }}
         />
       </div>
     </div>
